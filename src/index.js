6c4daa3cf1bbed491345723a65b6dfb9093c6c73
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-12 19:50:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 ); 
 
-// If you want to start measuring performance in your app, 
+// If you want to start measuring performance in your app, pass into function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals 
 reportWebVitals();
